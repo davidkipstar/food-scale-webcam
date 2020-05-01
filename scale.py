83d08b0c3574e8104113b09bada506b3e7101ce2
davidkipstar <david.arnold93@gmail.com>
2020-05-01 20:21:56 +0100

    scale is reading on pi

--- scale.py
+++ scale.py
@@ -4,9 +4,10 @@ import logging
 import os
 import pdb
 import asyncio
-import pandas as pd
 import numpy  as np
 import binascii
+import re
+import datetime
 
 from binascii import hexlify
 from pygatt.util import uuid16_to_uuid
@@ -16,7 +17,7 @@ logging.getLogger('pygatt').setLevel(logging.DEBUG)
 logging.getLogger().setLevel(logging.DEBUG)
 #note each time /start is called make pd data series 
 
-f = open('scale.csv')
+f = open('./_scale.csv', 'w')
 
 """
 
@@ -42,26 +43,19 @@ print(tabulate(df, headers='keys', tablefmt='psql'))
 
 """
 
-def _parse(s):
-    # # # # # # # # #
-    # # #  # # #3# # #
-    # # #   # # # # #
+def _parse(array):
     sign = 1
     unit = None
-    array = s.split('-')
 
     #number
-    print(array)
     k = array[3] #first  2 bits
     n = array[4] #second 2 bits
     m = array[5] #sign
-    print(f"k {k} , n {n}, m {m}")
 
     if m == '01':
         sign = -1
 
     i = int(k + n, 16)
-    print(f"{k}+{n} = {i}")
 
     if array[6] == '33':
         sign = -1
@@ -72,29 +66,37 @@ def _parse(s):
 
     else:
         unit = 'g'
-    return f"{i} {unit}"
+
+    return f"{i},{unit}"
+
 
 def handle_data(handle, value):
     """
     handle -- integer, characteristic read handle the data was received on
-    value -- bytearray, the data returned in the notification
+    value  -- bytearray, the data returned in the notification
     """
     try:
         #df = pd.Series()
         print(value)
-        s = hexlify(value, '-').decode()
-        s = _parse(s)
-        f.write(s+'\n')
+        s = hexlify(value).decode()
+        d = re.findall('..?', s)
+        s = _parse(d)
+        _s = '-'.join(d)
+        f.write(_s+', '+s+'\n')
         logging.info(s)
+
     except Exception as e:
         logging.error(f"handle_data: {e}")
 
 
-def _run(device, UUID = ""):
+def _run(device, UUID):
     try:
+        logging.info(f"subscribe to {UUID}")
         device.subscribe(UUID, callback = handle_data)
-        for i in range(10):
-            time.sleep(0.5)
+        for i in range(15):
+            time.sleep(1)
+        logging.info("stop now")
+
     except Exception as e:
         logging.error(e)
     finally:
@@ -102,16 +104,16 @@ def _run(device, UUID = ""):
 
 def _scale():
     try:
-        ADRESS = '03:B3:EC:8D:F0:F1'
-        #ADRESS = '03:B3:EC:8D:EF:16'
+        #ADRESS = '03:B3:EC:8D:F0:F1'
+        ADRESS = '03:B3:EC:8D:EF:16'
         UUID   = "0000ffb2-0000-1000-8000-00805f9b34fb"
-        #0000ffb2-0000-1000-8000-00805f9b34fb
+        #UUID = 0000ffb2-0000-1000-8000-00805f9b34fb
         adapter= pygatt.GATTToolBackend()
         adapter.start()
         time.sleep(1)
         device = adapter.connect(ADRESS, timeout = 10)
         #for uuid in self.device.discover_characteristics().keys():
-        _run(device, UUID = UUID)
+        _run(device, UUID)
 
     except Exception as e:
         logging.error(e)
@@ -119,9 +121,6 @@ def _scale():
     finally:
         adapter.stop()
         f.close()
-
-
-
 
 if __name__ == '__main__':
     _scale()
