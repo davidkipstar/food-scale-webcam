f4eff5633e89fc88fed075541a19383b04ccf482
davidkipstar <david.arnold93@gmail.com>
2020-05-01 14:37:05 +0200

    install files

---
+++ install_openv_pi.sh
@@ -0,0 +1,17 @@
+
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk2.0-dev libgtk-3-dev
+sudo apt-get install libatlas-base-dev gfortran
+
+sudo apt-get install python3-dev
+sudo apt-get install python3-pip
+
+pip3 install opencv-python
+
+sudo apt-get install libqtgui4
+sudo modprobe bcm2835-v4l2
+sudo apt-get install libqt4-test
+
---
+++ install_python_3.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python3
+sudo apt-get install python3-dev
+sudo apt-get install python3-pip
